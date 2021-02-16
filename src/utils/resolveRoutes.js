32d2fc401789e6ecdf982aa253d6a2aab648d83e
josearmandog02 <josearmandog02@gmail.com>
2021-02-16 17:32:54 -0400

    Created getHash and resolveRoutes to manage wich route will render

---
+++ src/utils/resolveRoutes.js
@@ -0,0 +1,7 @@
+const resolveRoutes = route => {
+    if(resolveRoutes.length <= 3) {
+        let validRoute = route === '/' ? route : '/:id'
+        return validRoute
+    }
+    return `/${route}`
+}
