cb725638d54b378cddc82c3f736239ebed1a2da1
josearmandog02 <josearmandog02@gmail.com>
2021-02-16 19:12:09 -0400

    Fixed Bugs

--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,7 +1,9 @@
-const resolveRoutes = route => {
-    if(resolveRoutes.length <= 3) {
+const resolveRoutes = (route) => {
+    if (route.length <= 3) {
         let validRoute = route === '/' ? route : '/:id'
         return validRoute
     }
     return `/${route}`
-}
+};
+  
+export default resolveRoutes
